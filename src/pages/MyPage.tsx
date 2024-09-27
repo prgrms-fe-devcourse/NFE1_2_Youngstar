d6707c4bbe450ee851758f3d9dc1c343ddbddf9b
gd06070 <zizibae0715@naver.com>
2024-09-27 12:03:16 +0900

    feat: 설정 아이콘, tap-content 추가

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,53 +1,32 @@
-// import useFetchUsers from "../hooks/useFetchUsers";
-import "../styles/Mypage.css"; // SCSS 파일 임포트
+import "../styles/css/Mypage.css"; // 임시 CSS 파일 임포트
 import profilePhoto from "../assets/chun_bong.png";
-import logo_header from "../assets/logo_header.png"
+import settingIcon from "../assets/Setting.svg";
 
 const MyPage = () => {
-  // const { data } = useFetchUsers();
-
   return (
-    <>
-      {/* <div>Mypage</div> */}
-      {/* <header>
-
-            </header>
-            <div className = 'profile'>
-                <div id = 'profile-photo'>
-                    <img src = {profilePhoto} alt = "profile-photo" id = "pro_photo"></img>
-                </div>
-            </div> */}
-
-      <div className="container">
-        <header className="header">
-          <img className="logo" src={logo_header} alt="로고" />
-          <div className="search-bar">
-            <span className="search-icon"></span>
-            <input className="search-input" type="text" placeholder="검색" />
-          </div>
-        </header>
-        <div className="profile-section">
-          <img
-            className="profile-image"
-            src={profilePhoto}
-            alt="Profile"
-          />
-          <h2 className="userid">chun_bong</h2>
-          <div>
-            <p className="introduce-text">자기소개란</p>
-          </div>
-          
-        </div>
-        <div className="tab-mypage">
-          <button className="tab">게시물</button>
-          <button className="tab">팔로잉</button>
-          <button className="tab">팔로워</button>
+    <div className="container">
+      <div className="setting-section">
+        <img className="setting" src={settingIcon} alt="Setting" />
+      </div>
+      <div className="profile-section">
+        <img className="profile-image" src={profilePhoto} alt="Profile" />
+        <h2 className="userid">chun_bong</h2>
+        <div>
+          <p className="introduce-text">
+            안녕 나는 박춘봉이다 여기는 자기소개문구 입력란이다. 자기소개 문구
+            제한은 50자이고 수정 가능하다
+          </p>
         </div>
       </div>
-
-      {/* <div className="mypage-container">Mypage</div> */}
-      {/* {data && (<div>{data[0]}</div>)} */}
-    </>
+      <div className="tab-section">
+        <button className="tab">게시물</button>
+        <button className="tab">팔로잉</button>
+        <button className="tab">팔로워</button>
+      </div>
+      <div className="tab-content">
+        <p>탭하면 보이는 공간</p>
+      </div>
+    </div>
   );
 };
 
