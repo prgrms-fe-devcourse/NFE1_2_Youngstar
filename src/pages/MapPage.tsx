c711f3b44ec9fea25d9f844c364269f56c94f321
gd06070 <zizibae0715@naver.com>
2024-10-02 11:19:12 +0900

    feat: 인포윈도우 임시 추가 및 css 적용

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from "react";
-import '../styles/scss/Map.scss';
+import "../styles/scss/Map.scss";
 
 declare global {
   interface Window {
@@ -16,18 +16,37 @@ function MapPage() {
 
     if (container) {
       // container가 존재할 때만 지도 생성
-      const options = {
+      const mapOptions = {
         center: new kakao.maps.LatLng(33.450701, 126.570667),
         level: 3,
       };
 
-      new kakao.maps.Map(container, options);
+      const map = new kakao.maps.Map(container, mapOptions);
+      const markerPosition = new kakao.maps.LatLng(33.45074, 126.570667);
+
+      const iwContent =
+        '<div style="padding:15px; margin-bottom:15px;">이곳은 포스팅 썸네일이미지와 사용자 아이디가 들어가려나</div>';
+      const iwPosition = new kakao.maps.LatLng(33.450701, 126.570667);
+      const marker = new kakao.maps.Marker({
+        position: markerPosition,
+      });
+
+      const infowindow = new kakao.maps.InfoWindow({
+        position: iwPosition,
+        content: iwContent,
+      });
+
+      infowindow.open(map, marker);
+      marker.setMap(map);
     }
   }, []);
 
   return (
     <div className="map-container">
       <div ref={mapRef} style={{ width: "500px", height: "500px" }}></div>
+      <div className="button-container">
+        <button>수정하기</button>
+      </div>
     </div>
   );
 }
