2a01585de2270b979d105e507999abfa99150081
장하준 <gkwns8098@janghajun-ui-noteubug.local>
2024-10-03 18:50:12 +0900

    feat:검색기능

--- src/hooks/useFetchUsers.ts
+++ src/hooks/useFetchUsers.ts
@@ -1,13 +1,16 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import post from '../types/Post';
-import Userpage from'../types/Userpage'
+// import post from '../types/Post';
+// import Userpage from'../types/Userpage'
+import  Search  from '../types/Search'
+
 
 export default function useFetchUsers() {
-  const [data, setData] = useState  <Userpage| null>(null);
+  const [data, setData] = useState  <Search[]| null>(null);
 
   // const url = "https://kdt.frontend.5th.programmers.co.kr:5001/posts/65a7e3d3ddb5911c0c0f9dfb"
-  const url = "https://kdt.frontend.5th.programmers.co.kr:5001/users/65af52230f190d3bd8fe81df"
+  // const url = "https://kdt.frontend.5th.programmers.co.kr:5001/users/65af52230f190d3bd8fe81df"
+  const url ="https://kdt.frontend.5th.programmers.co.kr:5006/search/all/근육몬최익"
 
   useEffect(() => {
     const fetchData = async () => {
--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,15 +1,13 @@
 import "../styles/scss/Mypage.scss"; 
 import profileImage from "../assets/chun_bong.png";
 import settingIcon from "../assets/Setting.svg";
-import useFetchUsers from "../hooks/useFetchUsers";
-import Userpage from "../types/Userpage";
+
 import { useState } from "react";
 import SetModal from "./SettingModal";
 
 const MyPage = () => {
-  // const [isModalOpen, setIsModalOpen] = useState(false); // 0928 - 모달 상태 관리 추가
-  const { data } = useFetchUsers ();  // 0928 - 모달 상태 관리 추가
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false); // 0928 - 모달 상태 관리 추가
+
   // 0928 - 모달 열기/닫기 함수 추가 
   const toggleModal = () => {
     setIsModalOpen(!isModalOpen);
@@ -22,7 +20,7 @@ const MyPage = () => {
       </div>
       <div className="profile-section">
         <img className="profile-image" src={profileImage} alt="Profile" />
-        <h2 className="user-id">{data?.fullName}</h2>
+        <h2 className="user-id">chun_bong</h2>
         <div>
           <p className="introduce-text">
             안녕 나는 박춘봉이다 여기는 자기소개문구 입력란이다. 자기소개 문구
